108d7a0aa191107d1a21f3f8019a2ab9907e2a95
gasperpre <49305355+gasperpre@users.noreply.github.com>
2023-05-13 16:40:55 +0200

    add goerli network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv");
+dotenv.config({path: __dirname + '/.env'});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/" + process.env.INFURA_ID,
+      accounts: [process.env.DEPLOYER_PK]
+    }
+  }
 };
