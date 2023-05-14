5b28588793e8f8526c8d0492f772d5a68a80d46b
6124437 <arjanvanhoogdalem@gmail.com>
2023-05-14 09:50:06 +0200

    added mumbai deployment

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ npx hardhat test
 ```
 npx hardhat run scripts/deploy.js --network <network-name>
 ```
+
+Deployed contracts:
+
+[Polygon Mumbai](https://mumbai.polygonscan.com/address/0xA05E010d250E8848Db98663F45FE3BF8080f6b4C).
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
 const dotenv = require("dotenv");
-dotenv.config({path: __dirname + '/.env'});
+dotenv.config({ path: __dirname + '/.env' });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks: {
     goerli: {
-      url: "https://goerli.infura.io/v3/" + process.env.INFURA_ID,
-      accounts: [process.env.DEPLOYER_PK]
+      url: "https://goerli.infura.io/v3/" + process.env.INFURA_API_KEY,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    polygon_mumbai: {
+      url: "https://polygon-mumbai.infura.io/v3/" + process.env.INFURA_API_KEY,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    goerli: {
+      url: "https://optimism-goerli.infura.io/v3/" + process.env.INFURA_API_KEY,
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
