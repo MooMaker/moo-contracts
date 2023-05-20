63b719c64d3b324b09cb63804bca2121f25bcc00
puzzle-rusher <puzzlerusher@gmail.com>
2023-05-20 12:07:05 +0400

    hardhatc config fixes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,36 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-verify");
+
 const dotenv = require("dotenv");
 dotenv.config({ path: __dirname + '/.env' });
 
+const infura_api_key = process.env.INFURA_API_KEY;
+const private_key = process.env.PRIVATE_KEY;
+
+if (!infura_api_key) {
+  throw new Error('Set INFURA_API_KEY in env');
+}
+
+if (!private_key) {
+  throw new Error('Set PRIVATE_KEY in env');
+}
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.18",
   networks: {
     goerli: {
-      url: "https://goerli.infura.io/v3/" + process.env.INFURA_API_KEY,
-      accounts: [process.env.PRIVATE_KEY]
+      url: "https://goerli.infura.io/v3/" + infura_api_key,
+      accounts: [private_key]
     },
     polygon_mumbai: {
-      url: "https://polygon-mumbai.infura.io/v3/" + process.env.INFURA_API_KEY,
-      accounts: [process.env.PRIVATE_KEY]
+      url: "https://polygon-mumbai.infura.io/v3/" + infura_api_key,
+      accounts: [private_key]
     },
     goerli: {
-      url: "https://optimism-goerli.infura.io/v3/" + process.env.INFURA_API_KEY,
-      accounts: [process.env.PRIVATE_KEY]
+      url: "https://optimism-goerli.infura.io/v3/" + infura_api_key,
+      accounts: [private_key]
     }
   }
 };
